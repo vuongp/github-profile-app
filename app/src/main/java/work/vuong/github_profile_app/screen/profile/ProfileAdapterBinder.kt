77320fb6bd004bc8de33c0675b405971b2e5048f
Vuong Pham <h.vuongpham@gmail.com>
2021-08-27 04:34:43 +0200

    A little refactoring

--- app/src/main/java/work/vuong/github_profile_app/screen/profile/ProfileAdapterBinder.kt
+++ app/src/main/java/work/vuong/github_profile_app/screen/profile/ProfileAdapterBinder.kt
@@ -17,139 +17,129 @@ import work.vuong.view_components.profile.TopBarTitleAdapter
 import javax.inject.Inject
 
 class ProfileAdapterBinder @Inject constructor(
-    private val resources: Resources
+    resources: Resources
 ): AdapterBinder<ConcatAdapter, GetUserQuery.User> {
 
+    private val materialPoint2 = resources.getDimensionPixelSize(R.dimen.material_point_2)
+    private val smallRepositoryWidth = resources.getDimensionPixelSize(R.dimen.small_repository_width)
+    private val horizontalRepositoryListDecorations = listOf(
+        MarginByTypeDecoration(
+            R.layout.repository_item,
+            left = materialPoint2
+        ),
+        LastItemDecoration(
+            right = materialPoint2
+        ),
+        SizeDecoration(
+            width = smallRepositoryWidth
+        )
+    )
+
+    private val topBarTitleAdapter = TopBarTitleAdapter()
+    private val profileHeaderAdapter = ProfileHeaderAdapter()
+    private val pinnedCategoryTitleAdapter = CategoryTitleAdapter()
+    private val pinnedRepositoryItemAdapter = RepositoryItemAdapter()
+    private val topCategoryTitleAdapter = CategoryTitleAdapter()
+    private val topRepositoryListAdapter = HorizontalRepositoryListAdapter(horizontalRepositoryListDecorations)
+    private val starredCategoryTitleAdapter = CategoryTitleAdapter()
+    private val starredRepositoryListAdapter = HorizontalRepositoryListAdapter(horizontalRepositoryListDecorations)
+
+    init {
+        topBarTitleAdapter.submitList(listOf(TopBarTitleAdapter.ViewItem(resources.getString(R.string.profile_title))))
+        pinnedCategoryTitleAdapter.submitList(listOf(CategoryTitleAdapter.ViewItem(resources.getString(R.string.pinned_repositories_title))))
+        topCategoryTitleAdapter.submitList(listOf(CategoryTitleAdapter.ViewItem(resources.getString(R.string.top_repositories_title))))
+        starredCategoryTitleAdapter.submitList(listOf(CategoryTitleAdapter.ViewItem(resources.getString(R.string.starred_repositories_title))))
+    }
+
     override fun bind(adapter: ConcatAdapter, item: GetUserQuery.User) {
-        // TODO: 27/08/2021 refactor
-        adapter.addAdapter(TopBarTitleAdapter().apply {
-            submitList(
-                listOf(
-                    TopBarTitleAdapter.ViewItem("Profile")
-                )
-            )
-        })
+        adapter.addAdapter(topBarTitleAdapter)
 
-        adapter.addAdapter(ProfileHeaderAdapter().apply {
-            submitList(
-                listOf(
-                    ProfileHeaderAdapter.ViewItem(
-                        item.name.orEmpty(),
-                        item.login,
-                        item.email,
-                        item.followers.totalCount,
-                        item.following.totalCount,
-                    ).apply {
-                        profileImageLoader = CoilImageLoader(item.avatarUrl.toString())
-                    }
-                )
-            )
-        })
-
-        adapter.addAdapter(CategoryTitleAdapter().apply {
-            submitList(
-                listOf(
-                    CategoryTitleAdapter.ViewItem(
-                        "Pinned"
-                    )
-                )
-            )
-        })
+        profileHeaderAdapter.submitList(listOf(createProfileHeaderViewItem(item)))
+        adapter.addAdapter(profileHeaderAdapter)
 
         val pinnedRepositories = item.pinnedItems.nodes?.mapNotNull { node ->
-            node?.asRepository?.let {
-                RepositoryItemAdapter.ViewItem(
-                    it.owner.login,
-                    it.name,
-                    it.description.orEmpty(),
-                    it.stargazerCount,
-                    it.primaryLanguage?.name.orEmpty(),
-                    it.primaryLanguage?.color.orEmpty(),
-                ).apply {
-                    repositoryImageLoader = CoilImageLoader(it.owner.avatarUrl.toString())
-                }
-            }
+            node?.asRepository?.let { createRepositoryViewItem(it) }
+        }.orEmpty()
+        pinnedRepositoryItemAdapter.submitList(pinnedRepositories)
+
+        if (pinnedRepositories.isNotEmpty()) {
+            adapter.addAdapter(pinnedCategoryTitleAdapter)
+            adapter.addAdapter(pinnedRepositoryItemAdapter)
         }
 
-        adapter.addAdapter(RepositoryItemAdapter().apply {
-            submitList(pinnedRepositories)
-        })
-
-        adapter.addAdapter(CategoryTitleAdapter().apply {
-            submitList(
-                listOf(
-                    CategoryTitleAdapter.ViewItem(
-                        "Top repositories"
-                    )
-                )
-            )
-        })
-
-        val materialPoint2 = resources.getDimensionPixelSize(R.dimen.material_point_2)
-        val smallRepositoryWidth = resources.getDimensionPixelSize(R.dimen.small_repository_width)
-        val horizontalRepositoryListDecorations = listOf(
-            MarginByTypeDecoration(
-                R.layout.repository_item,
-                left = materialPoint2
-            ),
-            LastItemDecoration(
-                right = materialPoint2
-            ),
-            SizeDecoration(
-                width = smallRepositoryWidth
-            )
-        )
-
         val topRepos = item.topRepositories.nodes?.mapNotNull { node ->
-            node?.let {
-                RepositoryItemAdapter.ViewItem(
-                    it.owner.login,
-                    it.name,
-                    it.description.orEmpty(),
-                    it.stargazerCount,
-                    it.primaryLanguage?.name.orEmpty(),
-                    it.primaryLanguage?.color.orEmpty(),
-                ).apply {
-                    repositoryImageLoader = CoilImageLoader(it.owner.avatarUrl.toString())
-                }
-            }
+            node?.let { createRepositoryViewItem(it) }
         }.orEmpty()
-        adapter.addAdapter(HorizontalRepositoryListAdapter(horizontalRepositoryListDecorations).apply {
-            submitList(listOf(HorizontalRepositoryListAdapter.ViewItem(topRepos)))
-        })
+        topRepositoryListAdapter.submitList(listOf(HorizontalRepositoryListAdapter.ViewItem(topRepos)))
 
-        adapter.addAdapter(CategoryTitleAdapter().apply {
-            submitList(
-                listOf(
-                    CategoryTitleAdapter.ViewItem(
-                        "Starred repositories"
-                    )
-                )
-            )
-        })
+        if (topRepos.isNotEmpty()) {
+            adapter.addAdapter(topCategoryTitleAdapter)
+            adapter.addAdapter(topRepositoryListAdapter)
+        }
 
         val starredRepos = item.starredRepositories.nodes?.mapNotNull { node ->
-            node?.let {
-                RepositoryItemAdapter.ViewItem(
-                    it.owner.login,
-                    it.name,
-                    it.description.orEmpty(),
-                    it.stargazerCount,
-                    it.primaryLanguage?.name.orEmpty(),
-                    it.primaryLanguage?.color.orEmpty(),
-                ).apply {
-                    repositoryImageLoader = CoilImageLoader(it.owner.avatarUrl.toString())
-                }
-            }
+            node?.let { createRepositoryViewItem(it) }
         }.orEmpty()
+        starredRepositoryListAdapter.submitList(listOf(
+            HorizontalRepositoryListAdapter.ViewItem(starredRepos)
+        ))
 
-        adapter.addAdapter(HorizontalRepositoryListAdapter(horizontalRepositoryListDecorations).apply {
-            submitList(
-                listOf(
-                    HorizontalRepositoryListAdapter.ViewItem(starredRepos)
-                )
-            )
-        })
+        if (starredRepos.isNotEmpty()) {
+            adapter.addAdapter(starredCategoryTitleAdapter)
+            adapter.addAdapter(starredRepositoryListAdapter)
+        }
+    }
+
+    private fun createProfileHeaderViewItem(item: GetUserQuery.User): ProfileHeaderAdapter.ViewItem {
+        return ProfileHeaderAdapter.ViewItem(
+            item.name.orEmpty(),
+            item.login,
+            item.email,
+            item.followers.totalCount,
+            item.following.totalCount,
+        ).apply {
+            profileImageLoader = CoilImageLoader(item.avatarUrl.toString())
+        }
+    }
+
+    // FIXME: 27/08/2021 have repository graphql classes share the same data
+    private fun createRepositoryViewItem(it: GetUserQuery.AsRepository): RepositoryItemAdapter.ViewItem {
+        return RepositoryItemAdapter.ViewItem(
+            it.owner.login,
+            it.name,
+            it.description.orEmpty(),
+            it.stargazerCount,
+            it.primaryLanguage?.name.orEmpty(),
+            it.primaryLanguage?.color.orEmpty(),
+        ).apply {
+            repositoryImageLoader = CoilImageLoader(it.owner.avatarUrl.toString())
+        }
+    }
+
+    private fun createRepositoryViewItem(it: GetUserQuery.Node1): RepositoryItemAdapter.ViewItem {
+        return RepositoryItemAdapter.ViewItem(
+            it.owner.login,
+            it.name,
+            it.description.orEmpty(),
+            it.stargazerCount,
+            it.primaryLanguage?.name.orEmpty(),
+            it.primaryLanguage?.color.orEmpty(),
+        ).apply {
+            repositoryImageLoader = CoilImageLoader(it.owner.avatarUrl.toString())
+        }
+    }
+
+    private fun createRepositoryViewItem(it: GetUserQuery.Node2): RepositoryItemAdapter.ViewItem {
+        return RepositoryItemAdapter.ViewItem(
+            it.owner.login,
+            it.name,
+            it.description.orEmpty(),
+            it.stargazerCount,
+            it.primaryLanguage?.name.orEmpty(),
+            it.primaryLanguage?.color.orEmpty(),
+        ).apply {
+            repositoryImageLoader = CoilImageLoader(it.owner.avatarUrl.toString())
+        }
     }
 
 }
